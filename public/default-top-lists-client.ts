7579c80dc09cc10725effac29fb09399f00550dc
Lukas Panni <lukas.panni@outlook.de>
2022-07-22 15:45:06 +0200

    Add local api-proxy
    proxy requests if fetching third-party domains is blocked

--- public/default-top-lists-client.ts
+++ public/default-top-lists-client.ts
@@ -2,8 +2,11 @@ import { TimeRange, TopArtistsResponse, TopListsClient, TopTracksResponse } from
 
 export class DefaultTopListsClient implements TopListsClient {
   private static baseUrl = 'https://api.spotify.com/v1/me/top/';
-  private static topArtistsUrl = DefaultTopListsClient.baseUrl + 'artists';
-  private static topTracksUrl = DefaultTopListsClient.baseUrl + 'tracks';
+  private static proxyBaseUrl = '/proxy-api/me/top/';
+  private topArtistsUrl = DefaultTopListsClient.baseUrl + 'artists';
+  private topTracksUrl = DefaultTopListsClient.baseUrl + 'tracks';
+  private proxyActive = false;
+  private fatalError = false;
 
   private accessToken: string;
 
@@ -20,16 +23,36 @@ export class DefaultTopListsClient implements TopListsClient {
     offset: number = 0
   ): Promise<TopArtistsResponse> {
     return (
-      (await this.fetchTopData<TopArtistsResponse>(DefaultTopListsClient.topArtistsUrl, timeRange, limit, offset)) ??
-      ({ items: [], total: 0 } as TopArtistsResponse)
+      (await this.wrapCall(() =>
+        this.fetchTopData<TopArtistsResponse>(this.topArtistsUrl, timeRange, limit, offset)
+      )) ?? ({ items: [], total: 0 } as TopArtistsResponse)
     );
   }
 
   public async getTopTracks(timeRange: TimeRange, limit: number = 10, offset: number = 0): Promise<TopTracksResponse> {
     return (
-      (await this.fetchTopData<TopTracksResponse>(DefaultTopListsClient.topTracksUrl, timeRange, limit, offset)) ??
+      (await this.wrapCall(() => this.fetchTopData<TopTracksResponse>(this.topTracksUrl, timeRange, limit, offset))) ??
       ({ items: [], total: 0 } as TopTracksResponse)
     );
+  }
+
+  private async wrapCall<T>(functionCall: () => Promise<T | undefined>): Promise<T | undefined> {
+    return functionCall().catch(async (error) => {
+      console.error('Error fetching data, switching to proxy', error);
+      this.switchToProxyFallback();
+      return functionCall().catch((error) => {
+        console.error('Error fetching data with proxy, aborting', error);
+        this.fatalError = true;
+        return undefined;
+      });
+    });
+  }
+
+  private switchToProxyFallback() {
+    if (this.proxyActive) return;
+    this.topArtistsUrl = DefaultTopListsClient.proxyBaseUrl + 'artists';
+    this.topTracksUrl = DefaultTopListsClient.proxyBaseUrl + 'tracks';
+    this.proxyActive = true;
   }
 
   private async fetchTopData<T>(
